239e1bd5ee383658d0c8ce7dd3795b811df10b97
Aarushi Kumar <71340243+aarux-11@users.noreply.github.com>
2023-10-29 01:52:50 +0530

    Update test_mysql.py

--- test_mysql.py
+++ test_mysql.py
@@ -1,8 +1,15 @@
+import streamlit as st
 import mysql.connector
+import pandas as pd
+
 
 # database connection
 mydb = mysql.connector.connect(host="localhost", user="root", password="pass11", database= "youtube_db", auth_plugin='mysql_native_password')
 mycursor = mydb.cursor(buffered=True)
+
+st.write("Here's our first attempt at using data to create a table:")
+data = [["Channel_Name", "Test from test repo"],["Channel_Id", "UC1234567890",]]
+st.table(data)
 
 # Query for creating table
 ArtistTableSql = """CREATE TABLE Artists(
@@ -10,7 +17,10 @@ ID INT(20) PRIMARY KEY AUTO_INCREMENT,
 NAME  CHAR(20) NOT NULL,
 TRACK CHAR(10))"""
 
-cursor.execute(ArtistTableSql)
+if st.button("Upload to SQL"):
+    with st.spinner('Please Wait for it...'):
+        cursor.execute(ArtistTableSql)
+        st.success("Upload to MogoDB successful !!")
 
 # some other statements  with the help of cursor
 mydb.close()
