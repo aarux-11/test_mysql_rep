9f3e57f5b65becaa05ca60c87115addc19a2eb99
Aarushi Kumar <71340243+aarux-11@users.noreply.github.com>
2023-10-29 02:01:39 +0530

    Update test_mysql.py

--- test_mysql.py
+++ test_mysql.py
@@ -2,11 +2,6 @@ import streamlit as st
 import mysql.connector
 import pandas as pd
 
-
-# database connection
-mydb = mysql.connector.connect(host="192.168.56.1", user="root", password="pass11", database= "youtube_db", auth_plugin='mysql_native_password')
-mycursor = mydb.cursor(buffered=True)
-
 st.write("Here's our first attempt at using data to create a table:")
 data = [["Channel_Name", "Test from test repo"],["Channel_Id", "UC1234567890",]]
 st.table(data)
@@ -16,6 +11,10 @@ ArtistTableSql = """CREATE TABLE Artists(
 ID INT(20) PRIMARY KEY AUTO_INCREMENT,
 NAME  CHAR(20) NOT NULL,
 TRACK CHAR(10))"""
+
+# database connection
+mydb = mysql.connector.connect(host="192.168.56.1", user="root", password="pass11", database= "youtube_db", auth_plugin='mysql_native_password')
+mycursor = mydb.cursor(buffered=True)
 
 if st.button("Upload to SQL"):
     with st.spinner('Please Wait for it...'):
